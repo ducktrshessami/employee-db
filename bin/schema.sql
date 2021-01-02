286ed24bda8117079d844b57ddf1ad64c0ce63db
ducktrshessami <jwee761@gmail.com>
2021-01-01 19:17:32 -0800

    force not null for all columns except manager_id

--- bin/schema.sql
+++ bin/schema.sql
@@ -3,19 +3,19 @@ create database employee_db;
 use employee_db;
 
 create table department_tb(
-    id int primary key auto_increment,
+    id int primary key auto_increment not null,
     name varchar(30) not null
 );
 
 create table role_tb(
-    id int primary key auto_increment,
+    id int primary key auto_increment not null,
     title varchar(30) not null,
-    salary decimal(10, 2),
+    salary decimal(10, 2) not null,
     department_id int not null
 );
 
 create table employee_tb(
-	id int primary key auto_increment,
+	id int primary key auto_increment not null,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int not null,
